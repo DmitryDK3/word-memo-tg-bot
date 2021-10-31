1f9c59d35859182747251f86dee0a84ed63f38f7
DmitryDK3 <dmitrydk3@gmail.com>
2021-10-31 18:06:29 +0300

    WMTGB-0: created empty spring boot project

--- src/main/resources/db/migration/V00002__Add_values_to_user_words_table.sql
+++ src/main/resources/db/migration/V00002__Add_values_to_user_words_table.sql
@@ -1,4 +1,3 @@
 insert into user_words values (1, 'table', 'стол');
 insert into user_words values (2, 'spoon', 'ложка');
 insert into user_words values (1, 'dog', 'собака');
-insert into user_words values (4, 'dog', 'собака');
