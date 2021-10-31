f8eb682880caf8879e76f778e31abbdae5079894
DmitryDK3 <dmitrydk3@gmail.com>
2021-10-31 17:46:55 +0300

    WMTGB-0: created empty spring boot project

---
+++ src/main/java/com/github/DmitryDK3/wordmemotgbot/WordMemoTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.DmitryDK3.wordmemotgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordMemoTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordMemoTgBotApplication.class, args);
+	}
+
+}
